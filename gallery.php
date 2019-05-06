8ad740e18a9306c5e06224e9334eedb1360da1cc
Jelle <jelletraa@gmail.com>
2019-05-07 01:10:12 +0200

    first commit

---
+++ gallery.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gallery</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css.map" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <style>
+      html, body {
+                
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }    
+        
+    </style>
+
+</head>
+<body>
+    <div style="position:absolute" class="homebutton"><a href="index.php" class="button is-hoovered is-medium is-dark">Home</a></div>
+    <center><div style="font-size:50px;" class="title">Project Gallery</div></center>
+
+    <div class="content is-large">
+
+  <center><p>Below you can see my projects from new to old that I made (which are offline), you can check the code on my Github..<br>
+  </p></center>
+  
+</div>
+<div class="columns is-multiline">
+  <div class="column is-one-third-desktop is-half-tablet">
+    <div class="card">
+        <div class="card-image">
+        <video width="600" height="auto" controls>
+        <source src="resources/vid/todo4u.mp4" type="video/mp4">
+            <div class="card-content is-overlay is-clipped">
+              <span class="tag is-info">
+                Photo Title
+              </span>       
+            </div>
+        </div>
+        <footer class="card-footer">
+            <h4 class="card-footer-item">
+              ToDo4U Tasklist <br>
+            </h4>
+            <a href="https://github.com/jtraa/TODO4U" class="card-footer-item">
+              Check Code
+            </a>
+        </footer>
+    </div>
+  </div>  
+  
+  <div class="column is-one-third-desktop is-half-tablet">
+    <div class="card">
+        <div class="card-image">
+        <video width="600" height="auto" controls>
+        <source src="resources/vid/vanmoof.mp4" type="video/mp4">
+            <div class="card-content is-overlay is-clipped">
+              <span class="tag is-info">
+                Photo Title
+              </span>       
+            </div>
+        </div>
+        <footer class="card-footer">
+            <h4 class="card-footer-item">
+              Ebike-Webshop <br>
+            </h4>
+            <a href="https://github.com/jtraa/Bikewebshop" class="card-footer-item">
+              Check Code
+            </a>
+        </footer>
+    </div>
+  </div>
+
+  <div class="column is-one-third-desktop is-half-tablet">
+    <div class="card">
+        <div class="card-image">
+        <video width="600" height="auto" controls>
+        <source src="resources/vid/wireframesbeershop.mp4" type="video/mp4">
+            <div class="card-content is-overlay is-clipped">
+              <span class="tag is-info">
+                Photo Title
+              </span>       
+            </div>
+        </div>
+        <footer class="card-footer">
+            <h4 class="card-footer-item">
+            Beer Webshop <br>
+            </h4>
+            <a href="https://github.com/jtraa/Beer-Webshop-Definitive" class="card-footer-item">
+              Check Code
+            </a>
+        </footer>
+    </div>
+  </div>
+
+  <div class="column is-one-third-desktop is-half-tablet">
+    <div class="card">
+        <div class="card-image">
+        <video width="600" height="auto" controls>
+        <source src="resources/vid/harrypotter.mp4" type="video/mp4">
+            <div class="card-content is-overlay is-clipped">
+              <span class="tag is-info">
+                Photo Title
+              </span>       
+            </div>
+        </div>
+        <footer class="card-footer">
+            <h4 class="card-footer-item">
+            Beer Webshop <br>
+            </h4>
+            <a href="https://github.com/jtraa/Database-Bookstore" class="card-footer-item">
+              Check Code
+            </a>
+        </footer>
+    </div>
+  </div>
+ 
+  <div class="column is-one-third-desktop is-half-tablet">
+    <div class="card">
+        <div class="card-image">
+        <video width="600" height="auto" controls>
+        <source src="resources/vid/braber.mp4" type="video/mp4">
+              <div class="card-content is-overlay is-clipped">
+              <span class="tag is-info">
+                Photo Title That is really long to show the clipping
+              </span>       
+            </div>
+        </div>
+        <footer class="card-footer">
+            <h4 class="card-footer-item">
+              First Project <br>
+        </h4>
+            <a href="https://github.com/jtraa/Hackathon-Site" class="card-footer-item">
+              Check Code
+            </a>
+        </footer>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Jelle Traa</title>
+
+        <!-- Fonts -->
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css.map" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                overflow:hidden;
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                display:flex;
+                text-align: center;
+                flex-direction: column;
+                margin-top: 220px;
+            }
+
+            .title {
+                font-size: 50px;
+            }
+            .subtitle {
+                font-size:30px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 80px;
+            }
+        </style>
+    </head>
+    <body>
+
+            <div class="content">
+                <div class="title">
+                    Welkom on the Landing Page of Jelle Traa,</div>
+                    <div class="subtitle m-b-md">
+                    Below you can see all my projects and a link to my GitHub with all the projects plus more!
+                    </div><br>
+                
+
+                <div class="links">
+                    
+                    <a target="_blank" href="http://portfolio.jelletraa.com">Portfolio</a>
+                    <a href="gallery.php">Gallery</a>
+                    <a target="_blank" href="http://github.com/jtraa">GitHub</a>
+                    <a target="_blank" href="http://booklist.jelletraa.com">Booklist</a>
+                    <a target="_blank" href="http://numberguesser.jelletraa.com">Numberguesser</a><br>
+                    <a target="_blank" href="http://loancalculator.jelletraa.com">Loancalculator</a>
+                    <a target="_blank" href="http://finance.jelletraa.com">ERP-online</a>
+                    <a target="_blank" href="http://weather.jelletraa.com">WeatherApplication</a>
+                    <a target="_blank" href="http://kalenderweek.jelletraa.com">CalendarApplication</a>
+                    <a target="_blank" href="http://meditation.jelletraa.com">MeditationApplication</a>
+                </div>
+            </div>
+    </body>
+</html>
